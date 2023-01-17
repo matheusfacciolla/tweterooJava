a3a4ec997e568aed427aa5ed98b51210de6e8a2d
matheusfacciolla <matheustassifacciolla@gmail.com>
2023-01-17 16:50:04 -0300

    initialize the project with spring

---
+++ src/main/java/com/tweterooapi/tweterooapi/TweterooapiApplication.java
@@ -0,0 +1,13 @@
+package com.tweterooapi.tweterooapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweterooapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweterooapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweterooapi/tweterooapi/TweterooapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweterooapi.tweterooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
